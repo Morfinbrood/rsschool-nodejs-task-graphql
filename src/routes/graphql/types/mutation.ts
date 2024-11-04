e58bba6ff147859d99cd6ee8bdff99cc80b9a7ff
Yury Staravoitau <Morfinall@mail.ru>
2024-11-05 02:50:16 +0300

    refactor mutations

--- src/routes/graphql/types/mutation.ts
+++ src/routes/graphql/types/mutation.ts
@@ -6,6 +6,7 @@ import {
 import { UserType } from './user.js';
 import { PostType } from './post.js';
 import { ProfileType } from './profile.js';
+import { UUIDType } from './uuid.js';
 import {
   CreateUserInputType,
   CreatePostInputType,
@@ -14,168 +15,30 @@ import {
   ChangeProfileInputType,
   ChangeUserInputType,
 } from './inputTypes.js';
-import { UUIDType } from './uuid.js';
+
+import { GraphQLContext } from './interfaces/interfaces.js';
 
 import {
-  User,
-  Profile,
-  Post
-} from '@prisma/client';
+  changeUser,
+  createUser,
+  deleteUser,
+  subscribeTo,
+  unsubscribeFrom,
+} from './resolvers/userResolvers.js';
 import {
-  ChangeUserDto,
-  ChangeProfileDto,
-  ChangePostDto,
-  CreateUserDto,
-  CreateProfileDto,
-  CreatePostDto,
-  GraphQLContext
-} from './interfaces/interfaces.js';
-
-const changeProfile = async (
-  parent: unknown,
-  args: { id: string; dto: ChangeProfileDto },
-  context: GraphQLContext
-): Promise<Profile> => {
-  const { id, dto } = args;
-  return context.prisma.profile.update({
-    where: { id },
-    data: dto,
-  });
-};
-
-const changeUser = async (
-  parent: unknown,
-  args: { id: string; dto: ChangeUserDto },
-  context: GraphQLContext
-): Promise<User> => {
-  const { id, dto } = args;
-  return context.prisma.user.update({
-    where: { id },
-    data: dto,
-  });
-};
-
-const changePost = async (
-  parent: unknown,
-  args: { id: string; dto: ChangePostDto },
-  context: GraphQLContext
-): Promise<Post> => {
-  const { id, dto } = args;
-  return context.prisma.post.update({
-    where: { id },
-    data: dto,
-  });
-};
-
-const createUser = async (
-  parent: unknown,
-  args: { dto: CreateUserDto },
-  context: GraphQLContext
-): Promise<User> => {
-  return context.prisma.user.create({
-    data: args.dto,
-  });
-};
-
-const createProfile = async (
-  parent: unknown,
-  args: { dto: CreateProfileDto },
-  context: GraphQLContext
-): Promise<Profile> => {
-  return context.prisma.profile.create({
-    data: args.dto,
-  });
-};
-
-const createPost = async (
-  parent: unknown,
-  args: { dto: CreatePostDto },
-  context: GraphQLContext
-): Promise<Post> => {
-  return context.prisma.post.create({
-    data: args.dto,
-  });
-};
-
-const deleteUser = async (
-  parent: unknown,
-  args: { id: string },
-  context: GraphQLContext
-): Promise<string> => {
-  const { id } = args;
-  await context.prisma.user.delete({
-    where: { id },
-  });
-  return `User ${id} deleted successfully.`;
-};
-
-const deletePost = async (
-  parent: unknown,
-  args: { id: string },
-  context: GraphQLContext
-): Promise<string> => {
-  const { id } = args;
-  await context.prisma.post.delete({
-    where: { id },
-  });
-  return `Post ${id} deleted successfully.`;
-};
-
-const deleteProfile = async (
-  parent: unknown,
-  args: { id: string },
-  context: GraphQLContext
-): Promise<string> => {
-  const { id } = args;
-  await context.prisma.profile.delete({
-    where: { id },
-  });
-  return `Profile ${id} deleted successfully.`;
-};
-
-const subscribeTo = async (
-  parent: unknown,
-  args: { userId: string; authorId: string },
-  context: GraphQLContext
-): Promise<string> => {
-  const { userId, authorId } = args;
-  await context.prisma.subscribersOnAuthors.create({
-    data: {
-      subscriberId: userId,
-      authorId: authorId,
-    },
-  });
-  return `Subscribed successfully`;
-};
-
-const unsubscribeFrom = async (
-  parent: unknown,
-  args: { userId: string; authorId: string },
-  context: GraphQLContext
-): Promise<string> => {
-  const { userId, authorId } = args;
-  await context.prisma.subscribersOnAuthors.delete({
-    where: {
-      subscriberId_authorId: {
-        subscriberId: userId,
-        authorId: authorId,
-      },
-    },
-  });
-  return `Unsubscribed successfully`;
-};
+  changePost,
+  createPost,
+  deletePost,
+} from './resolvers/postResolvers.js';
+import {
+  changeProfile,
+  createProfile,
+  deleteProfile,
+} from './resolvers/profileResolvers.js';
 
 export const MutationType: GraphQLObjectType<any, GraphQLContext> = new GraphQLObjectType<any, GraphQLContext>({
   name: 'Mutations',
   fields: (): Record<string, any> => ({
-    changeProfile: {
-      type: new GraphQLNonNull(ProfileType),
-      args: {
-        id: { type: new GraphQLNonNull(UUIDType) },
-        dto: { type: new GraphQLNonNull(ChangeProfileInputType) },
-      },
-      resolve: changeProfile,
-    },
     changeUser: {
       type: new GraphQLNonNull(UserType),
       args: {
@@ -184,14 +47,6 @@ export const MutationType: GraphQLObjectType<any, GraphQLContext> = new GraphQLO
       },
       resolve: changeUser,
     },
-    changePost: {
-      type: new GraphQLNonNull(PostType),
-      args: {
-        id: { type: new GraphQLNonNull(UUIDType) },
-        dto: { type: new GraphQLNonNull(ChangePostInputType) },
-      },
-      resolve: changePost,
-    },
     createUser: {
       type: new GraphQLNonNull(UserType),
       args: {
@@ -199,40 +54,12 @@ export const MutationType: GraphQLObjectType<any, GraphQLContext> = new GraphQLO
       },
       resolve: createUser,
     },
-    createProfile: {
-      type: new GraphQLNonNull(ProfileType),
-      args: {
-        dto: { type: new GraphQLNonNull(CreateProfileInputType) },
-      },
-      resolve: createProfile,
-    },
-    createPost: {
-      type: new GraphQLNonNull(PostType),
-      args: {
-        dto: { type: new GraphQLNonNull(CreatePostInputType) },
-      },
-      resolve: createPost,
-    },
     deleteUser: {
       type: new GraphQLNonNull(GraphQLString),
       args: {
         id: { type: new GraphQLNonNull(UUIDType) },
       },
       resolve: deleteUser,
-    },
-    deletePost: {
-      type: new GraphQLNonNull(GraphQLString),
-      args: {
-        id: { type: new GraphQLNonNull(UUIDType) },
-      },
-      resolve: deletePost,
-    },
-    deleteProfile: {
-      type: new GraphQLNonNull(GraphQLString),
-      args: {
-        id: { type: new GraphQLNonNull(UUIDType) },
-      },
-      resolve: deleteProfile,
     },
     subscribeTo: {
       type: new GraphQLNonNull(GraphQLString),
@@ -249,6 +76,52 @@ export const MutationType: GraphQLObjectType<any, GraphQLContext> = new GraphQLO
         authorId: { type: new GraphQLNonNull(UUIDType) },
       },
       resolve: unsubscribeFrom,
+    },
+
+    changePost: {
+      type: new GraphQLNonNull(PostType),
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangePostInputType) },
+      },
+      resolve: changePost,
+    },
+    createPost: {
+      type: new GraphQLNonNull(PostType),
+      args: {
+        dto: { type: new GraphQLNonNull(CreatePostInputType) },
+      },
+      resolve: createPost,
+    },
+    deletePost: {
+      type: new GraphQLNonNull(GraphQLString),
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: deletePost,
+    },
+
+    changeProfile: {
+      type: new GraphQLNonNull(ProfileType),
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+        dto: { type: new GraphQLNonNull(ChangeProfileInputType) },
+      },
+      resolve: changeProfile,
+    },
+    createProfile: {
+      type: new GraphQLNonNull(ProfileType),
+      args: {
+        dto: { type: new GraphQLNonNull(CreateProfileInputType) },
+      },
+      resolve: createProfile,
+    },
+    deleteProfile: {
+      type: new GraphQLNonNull(GraphQLString),
+      args: {
+        id: { type: new GraphQLNonNull(UUIDType) },
+      },
+      resolve: deleteProfile,
     },
   }),
 });
